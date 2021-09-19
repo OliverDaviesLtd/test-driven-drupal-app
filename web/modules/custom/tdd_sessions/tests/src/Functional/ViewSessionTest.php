4044f7fb28c2346659278a99986f8604c033cb86
Oliver Davies <oliver@oliverdavies.uk>
2021-09-19 22:04:31 +0100

    fix(sessions): only add speaker names to sessions

--- web/modules/custom/tdd_sessions/tests/src/Functional/ViewSessionTest.php
+++ web/modules/custom/tdd_sessions/tests/src/Functional/ViewSessionTest.php
@@ -11,10 +11,14 @@ final class ViewSessionTest extends BrowserTestBase {
   public $defaultTheme = 'stark';
 
   public static $modules = [
+    // Core.
     'filter',
     'node',
     'path',
+
+    // Custom.
     'tdd_sessions',
+    'tdd_sessions_test',
   ];
 
   /** @test */
@@ -34,6 +38,23 @@ final class ViewSessionTest extends BrowserTestBase {
     $assert->pageTextContains('Test Driven Drupal');
     $assert->pageTextContains('Oliver Davies');
   }
+
+  /** @test */
+  public function speaker_names_are_only_added_to_sessions(): void {
+    $speaker = $this->drupalCreateUser([], 'Oliver Davies');
+
+    $this->drupalCreateNode([
+      'type' => 'page',
+      'uid' => $speaker->id(),
+    ]);
+
+    $this->drupalGet('/node/1');
+
+    $assert = $this->assertSession();
+    $assert->statusCodeEquals(Response::HTTP_OK);
+    $assert->pageTextNotContains('Oliver Davies');
+  }
+
 
   private function createSession(array $values = []): NodeInterface {
     $defaults = [
